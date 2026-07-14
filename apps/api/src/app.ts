d9da8728981139421d70f6fac3838c4a827746c9
ragu-2909 <raghavendraashokkumbar@gmail.com>
2026-07-15 00:01:07 +0530

    apidone

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -23,7 +23,8 @@ const getAllowedOrigins = () => {
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:4173",
-    "http://127.0.0.1:4173"
+    "http://127.0.0.1:4173",
+    "https://foodfest-bloom-web-qfuh.vercel.app/"
   ];
 };
 
